71af3baa13f992af5c2007c020755fcca7b034cf
alhaifim <alhaifim@outlook.com>
2019-11-14 15:38:51 -0700

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom';
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+      <Route exact path='/' component={HomePage} /> {/* the / on the path means the root.  exact is a bolean true or false 
+      property and if nothing was written it will be true
+       which means the path must be exactly / in order to render*/}
+       <Route  path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
