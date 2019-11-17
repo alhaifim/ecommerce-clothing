2a5e5073d73c26f811f0e12b57b6543a91b8a619
alhaifim <alhaifim@outlook.com>
2019-11-16 21:42:39 -0700

    Adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
       <Route exact path='/' component={HomePage} /> {/* the / on the path means the root.  exact is a bolean true or false 
       property and if nothing was written it will be true
