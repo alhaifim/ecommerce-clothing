3438920e9be1114267a6255accab910097ae5290
alhaifim <alhaifim@outlook.com>
2019-11-17 22:48:18 -0700

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,42 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyCncxZfSG_x6JFD15qQ8JmBL1YQDvZ7VpE",
+  authDomain: "crwon-db.firebaseapp.com",
+  databaseURL: "https://crwon-db.firebaseio.com",
+  projectId: "crwon-db",
+  storageBucket: "crwon-db.appspot.com",
+  messagingSenderId: "927865281894",
+  appId: "1:927865281894:web:11c856a592d2cdba257ca7",
+  measurementId: "G-X4QYWQD624"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+if(!userAuth) return;
+
+const userRef = firestore.doc(`users/${userAuth.uid}`);
+const snapShot = await userRef.get();
+if(!snapShot.exists){
+  const {displayName, email} = userAuth;
+  const createdAt = new Date();
+
+  try {
+    await userRef.set({
+      displayName,
+      email, 
+      createdAt,
+      ...additionalData
+    })
+
+    } catch(error){
+    console.log('error creating user', error.message);
+
+    }
+  }
+  return userRef;
+};
+
+
 
 firebase.initializeApp(config);
 
